84bd3b90c902c9feacc02d636b33bdd7c17ba7c5
softforyou <softforyou247@gmail.com>
2024-01-04 10:29:22 +0900

    changeapiaddr

--- services/api-addr/index.ts
+++ services/api-addr/index.ts
@@ -1,7 +1,7 @@
 const useApiAddr = () => {
-  // return 'https://api.studio-seba.com';
+  return 'https://api.hockeytv.kr';
   // return 'http://192.168.0.36:3000';
-  return 'http://localhost:3000';
+  // return 'http://localhost:3000';
 };
 
 export default useApiAddr;
